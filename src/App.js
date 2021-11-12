85bf7cd23db3e8c19db75ac89b0de93939be1ce3
jagritisingh30 <94175297+jagritisingh30@users.noreply.github.com>
2021-11-12 21:32:37 +0530

    jagriti

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+import FrameImage from './FrameImage';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <FrameImage/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
